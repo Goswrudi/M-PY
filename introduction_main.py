a2200bb9a447cacd245b6c99be1c469cce1a10eb
Rudransh Goswami <rudranshgoswami415@gmail.com>
2026-06-27 15:28:09 +0530

    Added the introduction_main.py file

---
+++ introduction_main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# In python 
+
+# list = [1, 2, 3, 4 ]
+# print(list)
+
+# how do u mupltiply them using numpy array?
+
+array = np.array([1, 2, 3, 4])
+array = array * 2
+print(array)
+print(type(array)) 
